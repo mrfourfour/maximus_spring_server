a2194c872612adc816da568b816bb00931ef5c11
chimaek <pipiru100@gmail.com>
2021-05-11 15:32:23 +0900

    number 1 commit

---
+++ src/main/java/com/maximus/spring_server/controller/AccountController.java
@@ -0,0 +1,22 @@
+package com.maximus.spring_server.controller;
+
+
+import com.maximus.spring_server.dto.AccountSaveDto;
+import com.maximus.spring_server.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping("/account/signup")
+    public Long save(@RequestBody AccountSaveDto accountSaveDto) throws Exception {
+        return accountService.save(accountSaveDto);
+    }
+
+}
